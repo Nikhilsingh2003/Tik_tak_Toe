6bcc319a789c3d4b5d9772bf8d14e65ee6df7beb
Nikhil Singh <101012986+Nikhilsingh2003@users.noreply.github.com>
2023-05-31 18:06:24 +0530

    Create output.cpp
    output of the game

---
+++ output.cpp
@@ -0,0 +1,43 @@
+                                 > cd "c:\Users\HP\Desktop\New folder\" ; if ($?) { g++ bdcgr.cpp -o bdcgr } ; if ($?) { .\bdcgr }
+Welcome to Tic Tac Toe!
+Current Board:
+   1   2   3
+1    |   |
+   ---------
+2    |   |
+   ---------
+3    |   |
+Player X, enter your move (row column): 2
+2
+Current Board:
+   1   2   3
+1    |   |
+   ---------
+2    | X |
+   ---------
+3    |   |
+Player O, enter your move (row column): 1
+1
+Current Board:
+   1   2   3
+1  O |   |
+   ---------
+2    | X |
+   ---------
+3    |   |
+Player X, enter your move (row column): 3
+1
+Current Board:
+   1   2   3
+1  O |   |
+   ---------
+3  X |   |
+Player O, enter your move (row column): 1
+2
+Player O wins!
+   1   2   3
+1  O | O | O
+   ---------
+2  X | X |
+   ---------
+3  X |   |
